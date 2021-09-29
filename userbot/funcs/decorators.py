aecede6fb395d574f6cccc2438e787457b19ba7b
Okek <87618530+skuyykek69@users.noreply.github.com>
2021-09-29 21:44:30 +0700

    Update decorators.py

--- userbot/funcs/decorators.py
+++ userbot/funcs/decorators.py
@@ -22,7 +22,7 @@ def check_owner(func):
         else:
             HELP_TEXT = (
                 gvarstatus("HELP_TEXT")
-                or "Only My Master can Access This !!\n\n and this kekXBot Repository is private."
+                or "Only My Master can Access This !!\n\nthis kekXBot Repository is private."
             )
             await c_q.answer(
                 HELP_TEXT,
