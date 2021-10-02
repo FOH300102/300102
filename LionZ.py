f36042a1a2239b1ae5be2651e602b0e712f0af7e
FOH300102 <90313452+FOH300102@users.noreply.github.com>
2021-10-03 04:30:09 +0700

    Update LionZ.py

--- LionZ.py
+++ LionZ.py
@@ -7,7 +7,7 @@ logging.basicConfig(
 )
 # Starting bot
 system(
-    "git clone https://gitHub.com/FOH300102/300102 && cd LionZ && python3 -m userbot"
+    "git clone https://gitHub.com/FOH300102/300102 && cd 300102 && python3 -m userbot"
 )
 # if got error
 print(" Sʀʏʏ ᴜɴғᴏʀᴛᴜɴᴀᴛᴇʟʏ ʏᴏᴜ ʜᴀᴠᴇ ɢᴏᴛ ᴇʀʀᴏʀ ᴘʟᴇᴀsᴇ ʀᴇᴘᴏʀᴛ ɪᴛ ᴀᴛ @LionXsupport")
