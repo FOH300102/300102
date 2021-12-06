642fe40ba8638952a21d66d03479aacb29721eb4
FOH300102 <90313452+FOH300102@users.noreply.github.com>
2021-12-06 15:32:18 +0700

    Update bot_pms.py

--- userbot/assistant/bot_pms.py
+++ userbot/assistant/bot_pms.py
@@ -106,8 +106,8 @@ async def bot_start(event):
             start_msg = f"Hi Bastard! 👤{mention},\
                         \nI am {my_mention}'s assistant bot.\
                         \nYou can contact to my master from here.\
-                        \n\nPowered by [𝙆𝙀𝙆 𝕏 𝙋𝙧𝙤𝙟𝙚𝙘𝙩𝙨](https://t.me/KEK_Projects)"
-        buttons = [(Button.url("Github", "https://github.com/skuyykek69"),)]
+                        \n\n👨🏻‍💻 Powered by [𝙆𝙀𝙆 𝕏 𝙋𝙧𝙤𝙟𝙚𝙘𝙩𝙨](https://t.me/KEK_Projects)"
+        buttons = [(Button.url("Endorsement", "https://sociabuzz.com/iniokekk/tribe"),)]
     else:
         start_msg = "Hey Master!\
             \nHow can i help you ?"
